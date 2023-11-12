39891c90e6af9fb91bbfe3bf7ea17f58c0737905
Tafadzwa Marisa <122799195+marisahzw@users.noreply.github.com>
2023-11-12 15:24:05 -0500

    week09 react app

---
+++ labs/Lab7/labweek09/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+
+
+        <h1> Welcome to Fullstack Development - I </h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h3>101371922</h3>
+        <h3>Tafadzwa Marisa</h3>
+
+        <h4>George Brown College</h4>
+   
+
+
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
